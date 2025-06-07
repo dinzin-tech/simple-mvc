3568f9cb7905598fcd00a3bc095ea267b808f8c7
Mallikarjun <mallikarjun016.rymec@gmail.com>
2025-06-07 15:37:28 +0530

    console help file updated

--- src/console/commands/HelpCommand.php
+++ src/console/commands/HelpCommand.php
@@ -8,11 +8,15 @@ class HelpCommand
     {
         echo "Available commands:\n";
         echo "  --------------------------------------------------------------\n";
-        echo "  help                   - Show this help message\n";
+        echo "  help, -h               - Show this help message\n";
+        echo "  console:setup          - Setup console, load env variables\n";
         echo "  --------------------------------------------------------------\n";
         echo "  make:controller [name] - Create a new controller\n";
         echo "  make:model [name]      - Create a new model\n";
         echo "  make:view [name]       - Create a new view\n";
+        echo "  ---------------------------------------------------------------\n";
+        echo "  migrations:create      - Generate migration files\n";
+        echo "  migrations:run         - Run migrations\n";
         echo "  ---------------------------------------------------------------\n";
         echo "  migrations:create      - Generate migration files\n";
         echo "  migrations:run         - Run migrations\n";
